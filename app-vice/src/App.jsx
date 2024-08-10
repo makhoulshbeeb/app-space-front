ab32de431fee0664a39a550a411a314fe32e4723
hadishamoun1 <shamounhadi2@gmail.com>
2024-08-10 19:38:12 +0300

    Added signup page

--- app-vice/src/App.jsx
+++ app-vice/src/App.jsx
@@ -1,7 +1,18 @@
-import "./App.css";
-import Auth from "./pages/auth";
+
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Auth from "./pages/auth/index";
+import Signup from "./pages/auth/Signup";
+
 function App() {
-  return <Auth />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Auth />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ app-vice/src/pages/auth/Signup.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "./style.css";
+import image from "../../image/bg_image.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faLock } from "@fortawesome/free-solid-svg-icons"; // Import the specific icon
+
+const Auth = () => {
+  return (
+    <>
+      <div className="animated-circle"></div>
+
+      <div className="auth_container">
+        <div className="image_container">
+          <img src={image} className="image" alt="background" />
+        </div>
+        <div className="auth_form">
+          <p className="auth_title">Sign up</p>
+          <label>Name</label>
+          <div className="input-container">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <input type="email" placeholder="insert your name here.." />
+          </div>
+          <label>Email</label>
+          <div className="input-container">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <input type="email" placeholder="insert your email here.." />
+          </div>
+          <label>Password</label>
+          <div className="input-container">
+            <FontAwesomeIcon icon={faLock} className="icon" />
+            <input type="password" placeholder="insert your password here.." />
+          </div>
+          <label>Confirm Password</label>
+          <div className="input-container">
+            <FontAwesomeIcon icon={faLock} className="icon" />
+            <input type="password" placeholder="insert your password here.." />
+          </div>
+          <div className="checking">
+            <div className="checkbox">
+              <input type="checkbox" id="rememberMe" className="checkbox" />
+              <label htmlFor="rememberMe" className="checkbox-label">
+                Remember Me
+              </label>
+            </div>
+          </div>
+          <button className="login-button">Sign up</button>
+          <div className="checking-account">
+            
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Auth;
