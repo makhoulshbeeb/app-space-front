fba47347919c27c507b79a06c89722efdb582542
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-08-11 14:25:12 +0300

    added navbar

--- app-vice/src/App.jsx
+++ app-vice/src/App.jsx
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./components/navbar/navbar";
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
     </div>
   );
 }
---
+++ app-vice/src/components/navbar/navbar.jsx
@@ -0,0 +1,27 @@
+import "./navbar.css";
+import { FaSearch } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <div className="navbar-container">
+      <nav className="navbar">
+        <div className="logo">
+          <img src="/AppLogo.svg" alt="App Logo" />
+        </div>
+        <ul className="nav-links">
+          <li>Home</li>
+          <li>Discover</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+        </ul>
+        <div className="search-login-signup">
+          <FaSearch className="search-icon" />
+
+          <button className="signup-button">Sign Up</button>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
