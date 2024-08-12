c4714bce1274b5a58e2a2e7f1d9b6ae257492d0c
zaynabmamaa@gmail.com <zaynabmamaa@gmail.com>
2024-08-12 15:59:58 +0300

    fixed conflict

--- app-vice/src/App.jsx
+++ app-vice/src/App.jsx
@@ -1,3 +1,5 @@
+
+
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
@@ -5,18 +7,23 @@ import AboutUs from "./components/AboutUs";
 
 import ContactUs from "./components/ContactUs";
 import Signup from "./components/Signup";
-import Landing from "./components/LandingPage";
+// import Landing from "./components/LandingPage";
+import Dashboard from './components/Dashboard';
+import StreamlitPage from "./components/StreamlitPage";
+
+
 
 function App() {
   return (
     <Router>
       <Routes>
-      <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/about_us" element={<AboutUs />} />
         <Route path="/contact_us" element={<ContactUs />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/landing-page" element={<Landing />} />
+        {/* <Route path="/landing-page" element={<Landing />} /> */}
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/stream" element={<StreamlitPage />} />
       </Routes>
   
     </Router>
