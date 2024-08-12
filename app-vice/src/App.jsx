1596eb7eddafcf87ddb7331a8fb6e942c087deea
Abdallah <fafafavd@gmail.com>
2024-08-12 14:59:24 +0300

    Fixed categorylist css

--- app-vice/src/App.jsx
+++ app-vice/src/App.jsx
@@ -7,6 +7,7 @@ import ContactUs from "./components/ContactUs";
 import Signup from "./components/Signup";
 import Landing from "./components/LandingPage";
 import AppPage from "./components/AppPage";
+import Discover from "./components/Discover";
 
 function App() {
   return (
@@ -18,6 +19,8 @@ function App() {
         <Route path="/contact_us" element={<ContactUs />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/app/:id" element={<AppPage />} />
+        <Route path="/discover" element={<Discover />} />
+
       </Routes>
   
     </Router>
--- app-vice/src/components/Discover.jsx
+++ app-vice/src/components/Discover.jsx
@@ -1,8 +1,12 @@
 import AppList from "./AppList.jsx";
 import CategotiesList from "./CategoriesList.jsx";
+import Footer from "./Footer.jsx";
+import Navbar from "./Navbar.jsx";
 import "./styles/Discover.css"
 function Discover() {
   return (
+    <>
+    <Navbar/>
     <div className="discover">
     <div className="title">wondering which app to use?</div>
     <AppList title="Trending Apps"/>
@@ -10,6 +14,8 @@ function Discover() {
     <AppList title="Apps you’ll love"/>
     <CategotiesList/>
     </div>
+    <Footer/>
+    </>
   )
 }
 export default Discover
